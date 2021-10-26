d7eb3556a1bce1f25d77c2021de6a5fc695a4fb3
Denis Diveev <ddiveev99gmail.com>
2021-10-26 15:08:39 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <label v-for="item in 5" v-bind:key="item">
+        <input
+            type="radio"
+            v-bind:id="item"
+            v-bind:value="item"
+            v-model="picked"
+        />
+        {{ item }}
+      </label>
+      <hr/>
+      <h3>{{ currentMeetup }}</h3>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      API_URL: "https://course-vue.javascript.ru/api",
+      meetup: null,
+      picked: null,
+      events: [],
+    }
+  },
+  beforeMount() {
+    this.fetchMeetup()
+  },
+  computed: {
+    currentMeetup() {
+      return this.meetup ? this.meetup.title : ""
+    }
+  },
+  watch: {
+    picked(value) {
+      this.meetup = this.events[value]
+    }
+  },
+  methods: {
+    fetchMeetup() {
+      fetch(`${this.API_URL}/meetups`)
+          .then((response) => response.json())
+          .then((response) => {
+            this.events = response
+          })
+    },
   }
 }
 </script>
